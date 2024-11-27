c55917479fdd91ae36e6fed061785523e51496fd
Samuel Ridsdale <sam.ridsdale@netpointsolutions.co.uk>
2024-11-27 14:16:13 +0000

    back to top button added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 // Add custom JavaScript here
 function userScroll() {
     const navbar = document.querySelector('.navbar');
+    const toTop = document.querySelector('#to-top');
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
             navbar.classList.add('bg-light');
             navbar.classList.add('navbar-sticky');
+            toTop.classList.add('show');
         } else {
             navbar.classList.remove('bg-light');
             navbar.classList.remove('navbar-sticky');
+            toTop.classList.remove('show');
         }
     })
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 function incrementStats() {
@@ -40,3 +48,4 @@ function incrementStats() {
 
 document.addEventListener('DOMContentLoaded', userScroll);
 document.addEventListener('DOMContentLoaded', incrementStats);
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
