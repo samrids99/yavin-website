58a5cef95eb7f9e96b77355623d542b35f257216
Samuel Ridsdale <sam.ridsdale@netpointsolutions.co.uk>
2024-11-27 09:42:40 +0000

    intro details and services

--- js/script.js
+++ js/script.js
@@ -13,4 +13,30 @@ function userScroll() {
     })
 }
 
+function incrementStats() {
+    const counters = document.querySelectorAll('.counter');
+    const speed = 200;
+
+    counters.forEach((counter) => {
+        counter.innerText = '0';
+
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const c = +counter.innerText;
+
+            const increment = target / 200;
+
+            if(c < target) {
+                counter.innerText = Math.ceil(c + increment);
+                setTimeout(updateCounter, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCounter();
+    });
+}
+
 document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', incrementStats);
